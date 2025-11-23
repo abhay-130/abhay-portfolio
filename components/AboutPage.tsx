29437507c9230055c8c7561caec98f49d425f64a
Ar. Abhay Kishor <abhaykishor@Abhays-MacBook-Air-2.local>
2025-11-23 08:01:04 +0530

    updated about-page

--- components/AboutPage.tsx
+++ components/AboutPage.tsx
@@ -17,11 +17,11 @@ const AboutPage: React.FC = () => {
                     </p>
                 </div>
             </section>
-            {/* <div className="text-center mt-8">
+            <div className="text-center mt-8">
                 <Link to="/" className="text-[13px] mt-[20px] px-3 py-2 rounded-full font-semibold text-black dark:text-light-text transition-colors hover:bg-black hover:text-white dark:hover:bg-dark-text dark:hover:text-dark-bg">
                     Back to Home
                 </Link>
-            </div> */}
+            </div>
         </main>
     );
 };
