c915120b9ea1864f714415d1188ef16957be3e58
Abhay Kishor <abhaykishor130@gmail.com>
2025-11-29 00:26:01 +0530

    deploy from cursor

--- components/ContactPage.tsx
+++ components/ContactPage.tsx
@@ -1,6 +1,5 @@
 /// <reference types="vite/client" />
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
 const ContactPage: React.FC = () => {
   const [result, setResult] = useState("");
@@ -115,9 +114,6 @@ const ContactPage: React.FC = () => {
         </div>
         </section>
         <div className="text-center mt-8">
-            <Link to="/" className="text-[13px] mt-[20px] px-3 py-2 rounded-full font-semibold text-black dark:text-light-text transition-colors hover:bg-black hover:text-white dark:hover:bg-dark-text dark:hover:text-dark-bg">
-                Back to Home
-            </Link>
         </div>
     </main>
   );
